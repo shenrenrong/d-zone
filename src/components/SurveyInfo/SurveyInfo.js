31025487ee9a99831eee1706350267120b48c841
shenrenrong <charixiama@gmail.com>
2022-03-12 20:19:35 +0900

    create SurveyInfo folder

---
+++ src/components/SurveyInfo/SurveyInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./survey_info.css";
+
+function SurveyInfo() {
+  return (
+    <div className="survey_info">
+      
+    </div>
+  );
+}
+
+export default SurveyInfo;
