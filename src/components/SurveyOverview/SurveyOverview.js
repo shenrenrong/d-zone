6d6075803cd4028332deef83972a11f6fbd418a9
shenrenrong <charixiama@gmail.com>
2022-03-12 20:19:56 +0900

    create SurveyOverview folder

---
+++ src/components/SurveyOverview/SurveyOverview.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./survey_overview.css";
+
+function SurveyOverview() {
+  return (
+    <div className="survey_overview">
+      
+    </div>
+  );
+}
+
+export default SurveyOverview;
