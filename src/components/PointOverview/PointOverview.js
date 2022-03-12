0fef03fc6fccef0c31b036ecedee8080ee08c345
shenrenrong <charixiama@gmail.com>
2022-03-12 20:19:12 +0900

    create PointOverview folder

---
+++ src/components/PointOverview/PointOverview.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./point_overview.css";
+
+function PointOverview() {
+  return (
+    <div className="point_overview">
+      
+    </div>
+  );
+}
+
+export default PointOverview;
