9108bd64c866cc834dc4ec1b3f16c18e660840ef
shenrenrong <charixiama@gmail.com>
2022-03-12 20:18:22 +0900

    create PointInfo folder

---
+++ src/components/PointInfo/PointInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./point_info.css";
+
+function PointInfo() {
+  return (
+    <div className="point_info">
+      
+    </div>
+  );
+}
+
+export default PointInfo;
