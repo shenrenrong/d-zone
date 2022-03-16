c2d6d2834d8b1c28fe3f91621fa9eb217cacd10d
khy920723 <khy920723@gmail.com>
2022-03-17 01:18:48 +0900

    20220317 Loginbf_pannel3 js image file button script add

--- src/components/Loginbf_pannel3.js
+++ src/components/Loginbf_pannel3.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from './Loginbf_pannel3.module.css'
 
 function Loginbf_pannel3() {
-  return (
+    // button onClick
+    const handleInputFile=(inputId)=>{
+        let inputedId=document.getElementById(inputId);
+        inputedId.click();
+        document.getElementById('');
+    }
+
+
+    // image preview
+    const [imageSrc, setImageSrc] = useState('');
+    const encodeFileToBase64 = (fileBlob) => { 
+        const reader = new FileReader(); 
+        reader.readAsDataURL(fileBlob); 
+        
+        return new Promise((resolve) => { 
+            reader.onload = () => { 
+                setImageSrc(reader.result); 
+                resolve(); 
+            }; 
+        }); 
+    };
+
+  
+    return (
     <main>
       <h2 className={styles.pannelText}>패널인증시스템 신청</h2>
       <p>
@@ -72,16 +95,31 @@ function Loginbf_pannel3() {
           <div className={`${styles.row} ${styles.addfileRow}`}>
             <label htmlFor="">첨부파일</label>
             <div className={styles.addfileCol}>
-              <input type="file" id="addfile1" className="addfile1" style={{display:"none"}} />
-              <button className={styles.addfileBtn1}>
+              <input type="file" id="addfile1" className="addfile1" style={{display:"none"}} onChange={(e)=>{
+                  encodeFileToBase64(e.target.files[0]);
+              }} />
+              <button className={styles.addfileBtn1} onClick={(e)=>{
+                  e.preventDefault();
+                  handleInputFile("addfile1");
+              }}>
+                {imageSrc ? <img src={imageSrc} alt="preview-img" /> : <img src="img/addfile_camera.png" alt="" />}
+              </button>
+              <input type="file" id="addfile2" className="addfile2" style={{display:"none"}} onChange={(e)=>{
+                  encodeFileToBase64(e.target.files[0]);
+              }} />
+              <button className={styles.addfileBtn2} onClick={(e)=>{
+                  e.preventDefault();
+                  handleInputFile("addfile2");
+              }}>
                 <img src="img/addfile_camera.png" alt="" />
               </button>
-              <input type="file" id="addfile2" className="addfile2" style={{display:"none"}} />
-              <button className={styles.addfileBtn2}>
-                <img src="img/addfile_camera.png" alt="" />
-              </button>
-              <input type="file" id="addfile3" className="addfile3" style={{display:"none"}} />
-              <button className={styles.addfileBtn3}>
+              <input type="file" id="addfile3" className="addfile3" style={{display:"none"}} onChange={(e)=>{
+                  encodeFileToBase64(e.target.files[0]);
+              }} />
+              <button className={styles.addfileBtn3} onClick={(e)=>{
+                  e.preventDefault();
+                  handleInputFile("addfile3");
+              }}>
                 <img src="img/addfile_camera.png" alt="" />
               </button>
               <span className={styles.addfileDes}>
