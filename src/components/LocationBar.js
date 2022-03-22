5b0d5a1bea2a133f73f5e01cba5d66d22927a2c3
lazyreport <kinghyejoo@gmail.com>
2022-03-22 15:50:05 +0900

    formatting

--- src/components/LocationBar.js
+++ src/components/LocationBar.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 import "./LocationBar.css";
 
-function LocationBar({depth1, depth2}) {
+function LocationBar({ depth1, depth2 }) {
   return (
-    <div className='location-bar'>
-       <h3>{depth1}</h3> <span> &#62;
- </span> <h3>{depth2}</h3>
+    <div className="location-bar">
+      <h3>{depth1}</h3> <span> &#62;</span> <h3>{depth2}</h3>
     </div>
-  )
+  );
 }
 
-export default LocationBar
+export default LocationBar;
